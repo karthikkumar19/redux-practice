5ccefeeedb1e647858464aa6716dd3e7ebb87b6c
Karthik kumar <mkarthikkumar19@gmail.com>
2019-09-11 21:47:37 +0530

    redux connect to the react by using provider, connect

---
+++ src/containers/store/reducer.js
@@ -0,0 +1,27 @@
+const initialState={
+    counter:0
+}
+
+const reducer = (state = initialState, action) =>{
+    switch (action.type){
+        case 'INCREMENT':
+                return{
+                    counter: state.counter+1
+                }
+        case 'DECREMENT':
+                return{
+                    counter: state.counter-1
+                }
+        case 'ADDITION':
+                return{
+                    counter: state.counter+action.value
+                }
+        case 'SUBTRACTION':
+                return{
+                    counter: state.counter-action.value 
+                }
+    }
+    return state;
+}
+
+export default reducer;
